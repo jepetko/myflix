5efa4c1b9fbcd522286ab266c0c51691d1051cea
katarina <golbang.k@gmail.com>
2015-04-18 12:42:53 +0200

    exercise: build the home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def divide_in_chunks(model, size)
+    chunks = {}
+    chunk_idx = 0
+    model.each_with_index do |item,idx|
+      chunks[chunk_idx] ||= []
+      chunks[chunk_idx] << item
+      if (idx+1)%size == 0
+        chunk_idx=chunk_idx+1
+      end
+    end
+    chunks
+  end
+
 end
