41560bbc524c65981e1a600ad12d3b8c05f02155
katarina <golbang.k@gmail.com>
2015-04-20 21:06:03 +0200

    for now eliminate the beforeEachs

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -24,43 +24,38 @@ describe Video do
   describe '#search_by_title' do
 
     context 'the search term is family' do
-      context 'there are videos Family Guy, Family Affairs, Monk' do
+      context 'there are videos "Family Guy", "Family Affairs", "Monk"' do
+        it 'finds "Family Guy" and "Family Affairs"' do
+          family_guy = Video.create title: 'Family Guy', description: 'lorem ipsum'
+          family_affairs = Video.create title: 'Family Affairs', description: 'lorem ipsum'
+          monk = Video.create title: 'Monk', description: 'lorem ipsum'
 
-        before(:each) do
-          Video.create title: 'Family Guy', description: 'lorem ipsum'
-          Video.create title: 'Family Affairs', description: 'lorem ipsum'
-          Video.create title: 'Monk', description: 'lorem ipsum'
-        end
-
-        it 'finds Family Guy and Family Affairs' do
-          found_titles = Video.search_by_title('family').collect(&:title)
-          expect(found_titles).to include('Family Guy')
-          expect(found_titles).to include('Family Affairs')
+          expect(Video.search_by_title('family')).to eq([family_guy, family_affairs])
         end
       end
 
-      context 'there are videos Futurama and Boyhood' do
-
-        before(:each) do
-          Video.create title: 'Futurama', description: 'lorem ipsum'
-          Video.create title: 'Boyhood', description: 'lorem ipsum'
-        end
-
+      context 'there are videos "Futurama" and "Boyhood"' do
         it 'finds nothing' do
-          expect(Video.search_by_title('family')).to be_empty
+          futurama = Video.create title: 'Futurama', description: 'lorem ipsum'
+          boyhood = Video.create title: 'Boyhood', description: 'lorem ipsum'
+
+          expect(Video.search_by_title('family')).to eq([])
         end
       end
 
-      context 'there are videos Monk and My small family' do
-
-        before(:each) do
-          Video.create title: 'Monk', description: 'lorem ipsum'
-          Video.create title: 'My small family', description: 'lorem ipsum'
+      context 'there are videos "Monk" and "My small family"' do
+        it 'finds "My small family" as a partial match' do
+          monk = Video.create title: 'Monk', description: 'lorem ipsum'
+          my_small_family = Video.create title: 'My small family', description: 'lorem ipsum'
+          expect(Video.search_by_title('family')).to eq([my_small_family])
         end
+      end
 
-        it 'finds My small family' do
-          found_titles = Video.search_by_title('family').collect(&:title)
-          expect(found_titles).to include('My small family')
+      context 'there are videos "Monk" and "family"' do
+        it 'finds "family" as an exact match' do
+          monk = Video.create title: 'Monk', description: 'lorem ipsum'
+          family = Video.create title: 'family', description: 'lorem ipsum'
+          expect(Video.search_by_title('family')).to eq([family])
         end
       end
     end
