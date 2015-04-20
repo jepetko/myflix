9c0f4b2ba71ad05fe4e719667b86af73a426c593
katarina <golbang.k@gmail.com>
2015-04-20 20:54:13 +0200

    assignment: search_by_title

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,8 @@ class Video < ActiveRecord::Base
   belongs_to :category
 
   validates_presence_of :title, :description
+
+  def self.search_by_title(title)
+    Video.where("lower(title) LIKE '%#{title}%'")
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -21,4 +21,48 @@ describe Video do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
 
+  describe '#search_by_title' do
+
+    context 'the search term is family' do
+      context 'there are videos Family Guy, Family Affairs, Monk' do
+
+        before(:each) do
+          Video.create title: 'Family Guy', description: 'lorem ipsum'
+          Video.create title: 'Family Affairs', description: 'lorem ipsum'
+          Video.create title: 'Monk', description: 'lorem ipsum'
+        end
+
+        it 'finds Family Guy and Family Affairs' do
+          found_titles = Video.search_by_title('family').collect(&:title)
+          expect(found_titles).to include('Family Guy')
+          expect(found_titles).to include('Family Affairs')
+        end
+      end
+
+      context 'there are videos Futurama and Boyhood' do
+
+        before(:each) do
+          Video.create title: 'Futurama', description: 'lorem ipsum'
+          Video.create title: 'Boyhood', description: 'lorem ipsum'
+        end
+
+        it 'finds nothing' do
+          expect(Video.search_by_title('family')).to be_empty
+        end
+      end
+
+      context 'there are videos Monk and My small family' do
+
+        before(:each) do
+          Video.create title: 'Monk', description: 'lorem ipsum'
+          Video.create title: 'My small family', description: 'lorem ipsum'
+        end
+
+        it 'finds My small family' do
+          found_titles = Video.search_by_title('family').collect(&:title)
+          expect(found_titles).to include('My small family')
+        end
+      end
+    end
+  end
 end
