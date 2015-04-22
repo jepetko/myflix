73d9fc1ec90a64b5b92be58f3c103c32836c36c5
katarina <golbang.k@gmail.com>
2015-04-22 19:26:25 +0200

    refactory seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Category.create(name: 'TV Comedies')
-Category.create(name: 'TV Dramas')
-Category.create(name: 'Reality TV')
+comedies = Category.create(name: 'TV Comedies')
+dramas = Category.create(name: 'TV Dramas')
+reality = Category.create(name: 'Reality TV')
 
 Video.create(title: 'South Park', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut',
-             avatar: 'south_park.jpg', category: Category.find(1))
+             avatar: 'south_park.jpg', category: comedies)
 
 Video.create(title: 'Monk', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut',
-             avatar: 'monk.jpg', img: 'monk_large.jpg', category: Category.find(2))
+             avatar: 'monk.jpg', img: 'monk_large.jpg', category: dramas)
 
 Video.create(title: 'Family guy', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut',
-             avatar: 'family_guy.jpg', category: Category.find(3))
+             avatar: 'family_guy.jpg', category: reality)
 
 Video.create(title: 'Futurama', description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut',
-             avatar: 'futurama.jpg', category: Category.find(1))
+             avatar: 'futurama.jpg', category: comedies)
