bdf76f9741cd57e3d3234312f2205119e463e4de
katarina <golbang.k@gmail.com>
2015-04-23 19:07:08 +0200

    Assignment: front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,7 @@ class HomeController < ApplicationController
   def home
     @categories = Category.all
   end
+
+  def front
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+  root to: 'home#front'
 
   get 'home', controller: 'home'
 
+  resources :categories, only: [:show]
   resources :videos, only: [:index, :show] do
     collection do
       post :search, to: 'videos#search'
     end
   end
-  resources :categories, only: [:show]
 end
+
