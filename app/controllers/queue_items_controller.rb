d76d770935176921ee4bb6ed755b3e59d32dce40
katarina <golbang.k@gmail.com>
2015-05-30 11:44:00 +0200

    Push down complexity => move the methods to the model

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -7,20 +7,10 @@ class QueueItemsController < ApplicationController
   end
 
   def update_queue
-    QueueItem.transaction do
-      begin
-        ids = current_user.queue_items.all.map(&:id)
-        updateable_queue_items = get_updateable_queue_items params[:queue_items], ids
-        start_order_value = 1
-
-        updateable_queue_items.each do |updateable_queue_item|
-          current_queue_item = current_user.queue_items.find(updateable_queue_item[:id].to_i)
-          current_queue_item.update_attributes! order_value: start_order_value
-          start_order_value += 1
-        end
-      rescue Exception => e
-        flash[:error] = 'Queue items order not updated'
-      end
+    begin
+      current_user.update_queue_items params[:queue_items]
+    rescue Exception => e
+      flash[:error] = 'Queue items order not updated'
     end
     redirect_to my_queue_path
   end
@@ -65,15 +55,6 @@ class QueueItemsController < ApplicationController
   def video_queued?(video)
     queue_item = current_user.queue_items.build video_id: video.id, order_value: new_position
     queue_item.save
-  end
-
-  def get_updateable_queue_items(queue_items, ids)
-    queue_items.select { |item| ids.include?(item['id'].to_i) }.sort do |a,b|
-      first_order_value = a['order_value'].to_i
-      sec_order_value = b['order_value'].to_i
-      raise ArgumentError.new('order value must not be alphanumerical or zero') if first_order_value == 0 || sec_order_value == 0
-      a['order_value'].to_i-b['order_value'].to_i
-    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,29 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :full_name
   validates_uniqueness_of :email
 
+  def update_queue_items(queue_items_hash)
+    ActiveRecord::Base.transaction do
+      ids = queue_items.all.map(&:id)
+      updateable_queue_items = sort_updateable_queue_items queue_items_hash, ids
+      start_order_value = 1
+
+      updateable_queue_items.each do |updateable_queue_item|
+        current_queue_item = queue_items.find(updateable_queue_item[:id].to_i)
+        current_queue_item.update_attributes! order_value: start_order_value
+        start_order_value += 1
+      end
+    end
+  end
+
+  private
+
+  def sort_updateable_queue_items(queue_items, ids)
+    queue_items.select { |item| ids.include?(item['id'].to_i) }.sort do |a,b|
+      first_order_value = a['order_value'].to_i
+      sec_order_value = b['order_value'].to_i
+      raise ArgumentError.new('order value must not be alphanumerical or zero') if first_order_value == 0 || sec_order_value == 0
+      a['order_value'].to_i-b['order_value'].to_i
+    end
+  end
+
 end
