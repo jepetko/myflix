7d5fd4cd0354bfff277f36f252da25d702ae39eb
katarina <golbang.k@gmail.com>
2015-09-20 15:37:11 +0200

    fix tests after adding the before_filter

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -11,6 +11,10 @@ feature 'User page' do
   given!(:review) { Fabricate(:review, user: user, video: video_2, rating: 5) }
 
   background 'user page loaded' do
+    visit sign_in_path
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+    click_button 'Sign in'
     visit user_path(user)
   end
 
