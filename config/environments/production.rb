2fc53c569071a8d166fdb213364720f64578466b
katarina <golbang.k@gmail.com>
2015-10-01 20:40:23 +0200

    change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,9 +22,10 @@ Myflix::Application.configure do
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      domain:               'myflix.com',
+      domain:               'leanetic-myflix.herokuapp.com',
       user_name:            ENV['MAIL_USERNAME'],
       password:             ENV['MAIL_PASSWORD'],
       authentication:       'plain',
-      enable_starttls_auto: true  }
+      enable_starttls_auto: true
+  }
 end
