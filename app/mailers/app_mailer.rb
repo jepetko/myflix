eda8ee9a40ac263d6d7304fcb132e4be497bcea3
katarina <golbang.k@gmail.com>
2015-10-02 08:05:12 +0200

    change from address

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,7 +1,7 @@
 class AppMailer < ActionMailer::Base
 
   def send_mail(to, subject)
-    mail from: ENV['MAIL_USERNAME'], to: to, subject: subject
+    mail from: 'admin@myflix.com', to: to, subject: subject
   end
 
   def send_mail_on_register(user)
