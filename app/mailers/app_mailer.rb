2798d5059dfedb8554bae42536592e88ced8e864
katarina <golbang.k@gmail.com>
2015-10-02 08:15:57 +0200

    fix syntax error

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,7 +1,7 @@
 class AppMailer < ActionMailer::Base
 
   def send_mail(to, subject)
-    mail from: 'admin@myflix.com', to: to, subject: subject
+    mail from: 'admin@myflix.com', to: to.email, subject: subject
   end
 
   def send_mail_on_register(user)
