e5893dd20b99965530897f0f2cfb8b6b14f15702
katarina <golbang.k@gmail.com>
2015-10-19 11:47:06 +0200

    add staging environment

---
+++ config/environments/staging.rb
@@ -0,0 +1,28 @@
+Myflix::Application.configure do
+
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
+  config.serve_static_assets = false
+
+  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+
+  config.assets.compile = false
+
+  config.assets.digest = true
+
+  config.i18n.fallbacks = true
+
+  config.active_support.deprecation = :notify
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+      api_key: Rails.application.secrets.mail_api_key,
+      domain: Rails.application.secrets.mail_domain
+  }
+  config.action_mailer.default_url_options = {:host => 'leanetic-myflix.herokuapp.com'}
+
+  config.action_dispatch.show_exceptions = false
+end
