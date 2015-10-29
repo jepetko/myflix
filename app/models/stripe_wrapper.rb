5993913b61a7033778af416232cd73de2a5338ed
katarina <golbang.k@gmail.com>
2015-10-29 12:18:11 +0100

    Assignment: create subscriptions

--- app/models/stripe_wrapper.rb
+++ app/models/stripe_wrapper.rb
@@ -1,7 +1,13 @@
 module StripeWrapper
 
-  class Charge
+  module Base
+
+    def self.included(base)
+      base.extend StaticMethods
+    end
+
     attr_reader :status, :error_type, :error_message
+
     def initialize(status, options={})
       @status = status
       @error_type = options[:error_type]
@@ -12,6 +18,21 @@ module StripeWrapper
       @status == :success
     end
 
+    module StaticMethods
+      def handle_exception(err)
+        new :card_error, error_type: err.class.name, error_message: err.message
+      end
+    end
+
+  end
+
+  class Charge
+    include Base
+
+    def initialize(status, options={})
+      super status, options
+    end
+
     def self.create(options={})
       begin
         Stripe::Charge.create(card: options[:card],
@@ -20,7 +41,31 @@ module StripeWrapper
                               currency: 'usd')
         new :success
       rescue Stripe::CardError => err
-        new :card_error, error_type: err.class.name, error_message: err.message
+        handle_exception err
+      end
+    end
+
+
+  end
+
+  class Customer
+    include Base
+
+    attr_reader :customer_id
+
+    def initialize(status, options={})
+      super status, options
+      @customer_id = options[:customer_id]
+    end
+
+    def self.subscribe(options={})
+      begin
+        customer = Stripe::Customer.create( plan: 'standard',
+                                            source: options[:sources],
+                                            email: options[:user].email)
+        new :success, customer_id: customer.id
+      rescue Stripe::CardError => err
+        handle_exception err
       end
     end
 
--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
-describe StripeWrapper::Charge do
-
+describe StripeWrapper, :vcr do
   let(:token) {
     future = 12.month.from_now
     Stripe::Token.create(
@@ -14,26 +13,72 @@ describe StripeWrapper::Charge do
     ).id
   }
 
-  context 'for valid cards' do
+  describe 'StripeWrapper::Charge#create' do
 
-    let(:card_number) { '4242424242424242' }
-    it 'is successful', :vcr do
-      response = StripeWrapper::Charge.create(card: token, description: 'Myflix sign up charge')
-      expect(response).to be_successful
+    context 'for valid cards' do
+
+      let(:card_number) { '4242424242424242' }
+      it 'is successful' do
+        response = StripeWrapper::Charge.create(card: token, description: 'Myflix sign up charge')
+        expect(response).to be_successful
+      end
+
     end
 
+    context 'for invalid card number' do
+
+      let(:card_number) { '4000000000000002' }
+      it 'declines payment' do
+        response = StripeWrapper::Charge.create(card: token, description: 'Myflix sign up charge')
+        expect(response).not_to be_successful
+        expect(response.error_type).to eq 'Stripe::CardError'
+        expect(response.error_message).to eq 'Your card was declined.'
+      end
+
+    end
   end
 
-  context 'like invalid card number' do
+  describe 'StripeWrapper::Customer#subscribe' do
 
-    let(:card_number) { '4000000000000002' }
-    it 'declines payment', :vcr do
-      response = StripeWrapper::Charge.create(card: token, description: 'Myflix sign up charge')
-      expect(response).not_to be_successful
-      expect(response.error_type).to eq 'Stripe::CardError'
-      expect(response.error_message).to eq 'Your card was declined.'
+    let(:plan) {
+      Stripe::Plan.create(
+          :amount => 999,
+          :interval => 'month',
+          :name => 'myflix standard plan',
+          :currency => 'usd',
+          :id => 'standard'
+      )
+    }
+    let(:user) { Fabricate(:user) }
+
+    context 'valid credit card' do
+
+      let(:card_number) { '4242424242424242' }
+      before do
+        response = StripeWrapper::Customer.subscribe(source: token, user: user)
+      end
+
+      it 'subscribes a user for recurring payments' do
+        expect(response).to be_successful
+      end
+
+      it 'associates the customer id with the user' do
+        expect(user.stripe_id).to eq(response.customer_id)
+      end
     end
 
-  end
+    context 'invalid credit card' do
 
+      let(:card_number) { '4000000000000002' }
+      before do
+        response = StripeWrapper::Customer.subscribe(source: token, user: user)
+      end
+
+      it 'declines the subscription' do
+        expect(response).not_to be_successful
+        expect(response.error_type).to eq 'Stripe::CardError'
+        expect(response.error_message).to eq 'Your card was declined.'
+      end
+    end
+  end
 end
