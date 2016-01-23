d80b73d65b663f5a3ff565fb3bf79b4e6d2ad113
katarina <golbang.k@gmail.com>
2016-01-23 20:44:26 +0100

    disable metrics in test mode

--- page_metrics/lib/page_metrics.rb
+++ page_metrics/lib/page_metrics.rb
@@ -3,5 +3,5 @@ require "page_metrics/engine"
 module PageMetrics
   ActiveSupport::Notifications.subscribe 'process_action.action_controller' do |*args|
     PageMetrics::Metric.create name: args.first, payload: args.slice(1, args.size)
-  end
+  end unless Rails.env.test?
 end
