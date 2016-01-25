8ab93e7d285b6abdb0216aa98670e1e9a9e83f6a
katarina <golbang.k@gmail.com>
2016-01-25 21:49:06 +0100

    fix "\x89" from ASCII-8BIT to UTF-8 issue (because of the contained blob when postgres adapter tries to serialize it to json)

--- page_metrics/app/models/page_metrics/metric.rb
+++ page_metrics/app/models/page_metrics/metric.rb
@@ -1,4 +1,18 @@
 module PageMetrics
   class Metric < ActiveRecord::Base
+
+    def payload=(value)
+      value[3] = fix_params value[3]
+      write_attribute :payload, value
+    end
+
+    private
+
+    def fix_params(params)
+      params.each do |key,value|
+        params[key] = value.class.name if value && value.instance_variable_defined?('@original_filename')
+        fix_params value if value.is_a?(Hash)
+      end
+    end
   end
 end
